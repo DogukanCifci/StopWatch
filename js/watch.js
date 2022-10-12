70d2a13aa97a21f414934fb0a0fb6e3f04305fbb
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-13 00:58:21 +0200

    stop watch completed

--- js/watch.js
+++ js/watch.js
@@ -4,8 +4,8 @@ const imageReset = document.querySelector("img.reset");
 const milisecondsDisplay = document.querySelector("span.miliseconds");
 const secondDisplay = document.querySelector("span.seconds");
 const minuteDisplay = document.querySelector("span.minutes");
-console.log(imageReset);
-console.log(milisecondsDisplay);
+let milisecondsSayac = null;
+let secondSayac = null;
 let miliseconds = 0;
 let second = 0;
 let minutes = 0;
@@ -13,29 +13,28 @@ let minutes = 0;
 image.addEventListener("click", () => {
   if (image.src == "http://127.0.0.1:5500/images/start1.png") {
     image.src = "../images/pause.png";
-    const milisecondsSayac = setInterval(() => {
+    milisecondsSayac = setInterval(() => {
       miliseconds++;
       pressDisplay(milisecondsSayac);
     }, 10);
-    const secondSayac = setInterval(() => {
+    secondSayac = setInterval(() => {
       second++;
       pressDisplay(secondSayac);
     }, 1000);
   } else {
     image.src = "http://127.0.0.1:5500/images/start1.png";
+    clearInterval(milisecondsSayac);
+    clearInterval(secondSayac);
   }
 });
 
-imageReset.addEventListener("click", () => {
-  miliseconds = 0;
-  second = 0;
-  minutes = 0;
-  pressDisplay();
-});
-
 function pressDisplay() {
-  if (miliseconds == 100) miliseconds = 00;
-  milisecondsDisplay.textContent = miliseconds;
+  if (miliseconds == 100) {
+    milisecondsDisplay.textContent = "00";
+    miliseconds = 00;
+  }
+  if (miliseconds < 10) milisecondsDisplay.textContent = "0" + miliseconds;
+  else milisecondsDisplay.textContent = miliseconds;
   if (second < 10) secondDisplay.textContent = "0" + second;
   else secondDisplay.textContent = second;
   if (second == 60) {
@@ -46,4 +45,19 @@ function pressDisplay() {
     if (minutes < 10) minuteDisplay.textContent = "0" + minutes;
     else minuteDisplay.textContent = minutes;
   }
+}
+
+imageReset.onclick = () => {
+  reset();
+};
+function reset() {
+  clearInterval(milisecondsSayac);
+  clearInterval(secondSayac);
+  miliseconds = 0;
+  second = 0;
+  minutes = 0;
+
+  image.src = "../images/start1.png";
+
+  pressDisplay();
 }
