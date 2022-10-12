21a30d0cec5618fddb3c14f4bec55f58362e14c0
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-12 23:53:00 +0200

    pressDisplay function completed

--- js/watch.js
+++ js/watch.js
@@ -1,9 +1,10 @@
 //start-stop resimlerin tiklandiginda degismesi
 const image = document.querySelector("img.start-stop");
+const imageReset = document.querySelector("img.reset");
 const milisecondsDisplay = document.querySelector("span.miliseconds");
 const secondDisplay = document.querySelector("span.seconds");
 const minuteDisplay = document.querySelector("span.minutes");
-
+console.log(imageReset);
 console.log(milisecondsDisplay);
 let miliseconds = 0;
 let second = 0;
@@ -14,22 +15,35 @@ image.addEventListener("click", () => {
     image.src = "../images/pause.png";
     const milisecondsSayac = setInterval(() => {
       miliseconds++;
-      if (miliseconds == 100) miliseconds = 00;
-      milisecondsDisplay.textContent = miliseconds;
+      pressDisplay(milisecondsSayac);
     }, 10);
     const secondSayac = setInterval(() => {
       second++;
-      if (second < 10) secondDisplay.textContent = "0" + second;
-      else secondDisplay.textContent = second;
-      if (second == 60) {
-        secondDisplay.textContent = "00";
-        second = 0;
-        minutes++;
-        if (minutes < 10) minuteDisplay.textContent = "0" + minutes;
-        else minuteDisplay.textContent = minutes;
-      }
+      pressDisplay(secondSayac);
     }, 1000);
   } else {
     image.src = "http://127.0.0.1:5500/images/start1.png";
   }
 });
+
+imageReset.addEventListener("click", () => {
+  miliseconds = 0;
+  second = 0;
+  minutes = 0;
+  pressDisplay();
+});
+
+function pressDisplay() {
+  if (miliseconds == 100) miliseconds = 00;
+  milisecondsDisplay.textContent = miliseconds;
+  if (second < 10) secondDisplay.textContent = "0" + second;
+  else secondDisplay.textContent = second;
+  if (second == 60) {
+    secondDisplay.textContent = "00";
+    second = 0;
+    minutes++;
+
+    if (minutes < 10) minuteDisplay.textContent = "0" + minutes;
+    else minuteDisplay.textContent = minutes;
+  }
+}
